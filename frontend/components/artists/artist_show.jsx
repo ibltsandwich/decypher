a4b4f226d49f9daeafd1a7c6ceb33120217b91ac
Brian Lee <ibltsandwich@gmail.com>
2019-01-17 13:02:30 -0500

    Change routes for song'

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -4,14 +4,31 @@ import React from 'react';
 class ArtistShow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.fetchArtist(this.props.artistId).then(
+      response => this.setState({artist: response.payload.artist})
+    )
   }
   
   render() {
+    console.log(this.state.artist)
     const color ={
       color: 'white'
     }
     return (
-      <h1 style={color}>This is the artist show page</h1>
+      <div className="song-header-container">
+          <div className="song-album-img">
+
+          </div>
+        <h1 >This is the artist show page</h1>
+        {this.state.artist ? 
+        <h1>{this.state.artist.name}</h1> :
+        null }
+        <h1>COMING SOON</h1>
+      </div>
     )
   }
 }
--- frontend/components/artists/artist_show_container.js
+++ frontend/components/artists/artist_show_container.js
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux'
 import ArtistShow from './artist_show'
+import { fetchArtist } from '../../actions/artist_actions';
 
 const msp = (state, ownProps) => {
   return {
-    
-  }
+    artistId: ownProps.match.params.artistId
+  };
 }
 
 const mdp = dispatch => {
   return {
-
-  }
+    fetchArtist: id => dispatch(fetchArtist(id))
+  };
 }
 
 export default connect(msp, mdp)(ArtistShow)
