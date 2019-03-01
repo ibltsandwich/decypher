8d9e6a0a535912c43e788924dc5f900d3a054023
Brian Lee <ibltsandwich@gmail.com>
2019-03-01 15:51:45 -0500

    Truncate artist bio

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 class ArtistShow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {bioShortened: true};
+    this.openBio = this.openBio.bind(this);
   }
 
   componentDidMount() {
@@ -12,17 +13,42 @@ class ArtistShow extends React.Component {
       response => this.setState({artist: response.payload.artist})
     )
   }
+
+  openBio(e) {
+    e.preventDefault();
+    this.setState({bioShortened: false})
+  }
   
   render() {
     const color ={
       color: 'white'
     }
     if (this.state.artist) {
-      let bio = this.state.artist.bio.split("\n").map((paragraph, idx) => {
+      let shortenedBio;
+      let bio;
+      bio = this.state.artist.bio.split("\n").map((paragraph, idx) => {
         if (paragraph.length > 0) {
           return <p key={idx}>{paragraph}</p>
+        } else {
+          return null;
         }
       })
+
+      if (this.state.artist.bio.length > 325) {
+        let shortened = this.state.artist.bio.slice(0,325).split("\n")
+        shortenedBio = shortened.map((paragraph, idx) => {
+          if (idx === shortened.length - 1) {
+            return <p key={idx}>
+                    {paragraph}...
+                    <span key="10000" className="open-bio" onClick={this.openBio}> read more >></span>
+                   </p>
+          } else {
+            return <p key={idx}>{paragraph}</p>
+          }
+        })
+        // shortenedBio.push(<span key="10000" className="open-bio" onClick={this.openBio}> read more >></span>)
+      };
+
     return <div className="artist-show-container">
         <div className="artist-header-img" style={{ backgroundImage: `url(${this.state.artist.header_img})` }}>
           <div className="artist-header-container">
@@ -40,11 +66,14 @@ class ArtistShow extends React.Component {
             <h1 className="artist-name">
               {this.state.artist.name}
             </h1>
-            <div className="artist-bio">
+            <div className="artist-bio" id="shortened" ref={elem => this.artistBio = elem}>
               <h3>
                 About "{this.state.artist.name}"
               </h3>
-              {bio}
+              {this.state.bioShortened ? 
+                shortenedBio
+                : 
+                bio}
             </div>
           </section>
           <section className="artist-song-info">
